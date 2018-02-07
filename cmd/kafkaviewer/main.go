e85c8b046f7d3ae725837b0352b6c77ee6488b7b
Austin Roberts <code@ausiv.com>
2018-02-07 14:33:20 -0600

    Add kafkaviewer tool for viewing kafka logs

---
+++ cmd/kafkaviewer/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "github.com/ethereum/go-ethereum/ethdb/cdc"
+  "fmt"
+  "os"
+  "os/signal"
+  "log"
+)
+
+func main() {
+  consumer, err := cdc.NewKafkaLogConsumerFromURLs(os.Args[1:2], "geth", 0)
+  if err != nil {
+    log.Fatalf("%v", err.Error())
+  }
+  c := make(chan os.Signal, 1)
+  messages := consumer.Messages()
+	signal.Notify(c, os.Interrupt)
+  for {
+    select {
+    case op := <-messages:
+      log.Printf("%v\n", op)
+    case _ = <-c:
+      break
+    }
+    fmt.Scanln()
+  }
+}
--- ethdb/cdc/operation.go
+++ ethdb/cdc/operation.go
@@ -14,6 +14,8 @@ const (
   OpWrite byte = 2
   OpGet byte = 3
   OpHas byte = 4
+  OpBlockStart byte = 5
+  OpBlockEnd byte = 6
 )
 
 
@@ -22,6 +24,20 @@ type KeyValue struct {
   Value []byte
 }
 
+func (kv *KeyValue) String() (string) {
+  return fmt.Sprintf("KV{%#x(%v): %#x}", kv.Key, string(kv.Key), kv.Value)
+}
+
+type KVs []KeyValue
+
+func (kvs KVs) String() string {
+  output := "["
+  for _, kv := range kvs {
+    output += fmt.Sprintf("\n%v,", &kv)
+  }
+  output += "\n]"
+  return output
+}
 
 type Operation struct {
   Op byte
@@ -30,23 +46,29 @@ type Operation struct {
   Topic string
 }
 
-func (op *Operation) Apply(db ethdb.Database) error {
+func updateOffset(putter ethdb.Putter, op *Operation) {
   if op.Offset != 0 {
     buf := make([]byte, binary.MaxVarintLen64)
     binary.PutVarint(buf, op.Offset)
-    db.Put(
+    putter.Put(
       []byte(fmt.Sprintf("cdc-log-%v-offset", op.Topic)),
       buf,
     )
   }
+}
+func (op *Operation) Apply(db ethdb.Database) error {
   switch op.Op {
   case OpPut:
+    batch := db.NewBatch()
     kv := &KeyValue{}
     if err := rlp.DecodeBytes(op.Data, kv); err != nil { return err }
-    if err := db.Put(kv.Key, kv.Value); err != nil { return err }
+    if err := batch.Put(kv.Key, kv.Value); err != nil { return err }
+    updateOffset(batch, op)
+    if err := batch.Write(); err != nil { return err }
   case OpDelete:
     // For OpDelete, op.Data is the key to be deleted
     db.Delete(op.Data)
+    updateOffset(db, op)
   case OpWrite:
     batch := db.NewBatch()
     var puts []KeyValue
@@ -54,6 +76,7 @@ func (op *Operation) Apply(db ethdb.Database) error {
     for _, kv := range puts {
       if err := batch.Put(kv.Key, kv.Value); err != nil { return err }
     }
+    updateOffset(batch, op)
     if err := batch.Write(); err != nil { return err }
   }
   return nil
@@ -62,6 +85,30 @@ func (op *Operation) Apply(db ethdb.Database) error {
 func (op *Operation) Bytes() ([]byte) {
   data := []byte{op.Op}
   return append(data, op.Data...)
+}
+
+func (op *Operation) String() (string) {
+  switch op.Op {
+  case OpPut:
+    kv := &KeyValue{}
+    if err := rlp.DecodeBytes(op.Data, kv); err != nil { return err.Error() }
+    return fmt.Sprintf("PUT: %v", kv)
+  case OpDelete:
+    return fmt.Sprintf("DEL: %v", string(op.Data))
+  case OpWrite:
+    var puts []KeyValue
+    rlp.DecodeBytes(op.Data, &puts)
+    return fmt.Sprintf("WRITE: %v", KVs(puts))
+  case OpBlockStart:
+    var blocknum uint64
+    rlp.DecodeBytes(op.Data, &blocknum)
+    return fmt.Sprintf("BLOCKSTART: %v", blocknum)
+  case OpBlockEnd:
+    var blocknum uint64
+    rlp.DecodeBytes(op.Data, &blocknum)
+    return fmt.Sprintf("BLOCKEND: %v", blocknum)
+  }
+  return "UNKNOWN"
 }
 
 func OperationFromBytes(data []byte, topic string, offset int64) (*Operation, error) {
@@ -104,4 +151,16 @@ func GetOperation(key []byte) (*Operation, error) {
 
 func HasOperation(key []byte) (*Operation, error) {
   return &Operation{OpHas, key, 0, ""}, nil
+}
+
+func BlockStartOperation(blockNum uint64) (*Operation, error) {
+  data, err := rlp.EncodeToBytes(blockNum)
+  if err != nil { return nil, err }
+  return &Operation{OpBlockStart, data, 0, ""}, nil
+}
+
+func BlockEndOperation(blockNum uint64) (*Operation, error) {
+  data, err := rlp.EncodeToBytes(blockNum)
+  if err != nil { return nil, err }
+  return &Operation{OpBlockEnd, data, 0, ""}, nil
 }
